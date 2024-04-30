c0ff962e3bc164c505ef13fa7a464127c409620d
shannqa <116514532+Shannqa@users.noreply.github.com>
2024-04-30 16:44:41 +0200

    Change to ES6 modules, set connection to the database

---
+++ server/config/db.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default function connectDB() {
+  const url = process.env.DB_STRING;
+
+  try {
+    mongoose.connect(url);
+  } catch (err) {
+    console.error(err.message);
+  }
+
+  const dbConnection = mongoose.connection;
+
+  dbConnection.once("open", () => {
+    console.log("Database connected.");
+  });
+
+  dbConnection.on("error", (err) => {
+    console.error(`Dabatase connection error ${err}`);
+  });
+}
+
+// mongoose.set("strictQuery", false);
